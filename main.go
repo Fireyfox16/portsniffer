289ad28319a5fbfe338fbc53c8596de7c8d48170
René van Thoor <r.vanthoor@student.fontys.nl>
2024-02-26 13:40:37 +0100

    Added config and commited main

--- main.go
+++ main.go
@@ -1,25 +1,41 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net"
+	"os"
 	"time"
 )
 
 func main() {
-	var IP string
-	ports := []string{"21", "22", "25", "80", "443", "3306"}
+	var ports []string
+	var host string
+
+	file, err := os.Open("config.json")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+	jsonBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Println("Error reading file content:", err)
+		return
+	}
+	err = json.Unmarshal(jsonBytes, &ports)
+	if err != nil {
+		fmt.Println("Error decoding JSON:", err)
+		return
+	}
 
 	fmt.Println("Please type the IP address you want to scan:")
-	fmt.Scanln(&IP)
+	fmt.Scanln(&host)
 
-	checkPort(IP, ports)
-}
-
-func checkPort(host string, ports []string) {
 	for _, port := range ports {
-		timeout := time.Second
-		conn, err := net.DialTimeout("tcp", net.JoinHostPort(host, port), timeout)
+		_, err = connectPort(host, port)
+		conn, _ := connectPort(host, port)
 		if err != nil {
 			fmt.Println("Connecting error:", err)
 		}
@@ -28,4 +44,11 @@ func checkPort(host string, ports []string) {
 			fmt.Println("Opened", net.JoinHostPort(host, port))
 		}
 	}
+
+}
+
+func connectPort(host string, port string) (net.Conn, error) {
+	timeout := time.Second
+	conn, err := net.DialTimeout("tcp", net.JoinHostPort(host, port), timeout)
+	return conn, err
 }
